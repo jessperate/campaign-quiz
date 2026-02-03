da96696a22553b94ddf949c5b7de7a76af0ac9bd
Jess Rosenberg <jessrosenberg@Jesss-MBP.lan>
2026-02-03 13:35:32 -0600

    Clean up header styling

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 
 export function Header() {
   return (
-    <header className="fixed top-4 left-0 right-0 z-50 flex justify-center px-4">
+    <header className="fixed top-4 left-0 right-0 z-50 flex justify-center px-4 pointer-events-none">
       <Image
         src="/images/nav-bar.svg"
         alt="AirOps Navigation"
         width={1344}
         height={88}
-        className="w-auto h-[60px] md:h-[70px] lg:h-[88px] max-w-full"
+        className="w-auto h-[60px] md:h-[70px] lg:h-[88px] max-w-full pointer-events-auto"
         priority
       />
     </header>
